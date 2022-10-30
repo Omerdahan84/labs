11c7fa0413144d4f51534c45cb272cab5fa2c1bd
omer.dahan <omer111dahan@gmail.com>
2022-10-30 12:58:04 +0200

    Added an quiz from lab2

---
+++ lab1/lab1.py
@@ -0,0 +1,9 @@
+#############################################################
+# FILE : lab1.py
+# WRITER : your_name , your_login , your_id
+# EXERCISE : intro2cs1 lab1 2023
+# DESCRIPTION: A simple program that prints "Hello World!" to
+# the standard output (screen).
+#############################################################
+
+print("Hello World!")
---
+++ lab2-1.py
@@ -0,0 +1,15 @@
+def exp(base, power = 2):
+    return base ** power
+
+def calc(num, val):
+    if val == 'a':
+        return exp(exp(num))
+    elif val == 'b':
+        res = exp(num, 3)
+        if res <= 100:
+            return res
+    else:
+        return 'Illegal value'
+
+if __name__ == '__main__':
+    print(calc(5, 'a'))
